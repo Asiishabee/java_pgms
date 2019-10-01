faf5731cfd212af93bd52a1e62260c0894a9542a
Asiishabee <42670433+Asiishabee@users.noreply.github.com>
2019-10-01 12:00:38 +0530

    Create ArrayDemo.java

---
+++ ArrayDemo.java
@@ -0,0 +1,17 @@
+    
+public class ArrayDemo
+{
+ public static void main(String[] args)
+ {
+     int num[]=new int[5];
+     num[0]=1;
+     num[1]=2;
+     num[2]=3;
+     num[3]=4;
+     
+     for(int i=0;i<5;i++)
+     {
+     System.out.println(num[i]);
+     }
+ }   
+}
